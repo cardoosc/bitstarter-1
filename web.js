0069f686aa5e757dad1edeca10141dc3f83a9281
cardoos_c <chevisc@gmail.com>
2013-07-11 23:04:00 -0400

    edited web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
+var resp = fs.readFileSync('index.html', function(err ,data){
+	if(err) throw err;
+	console.log(data);
+    })
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2 !');
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
