e4cf1b617c00ae6c4141fc1f4a17d23b9b76133c
cardoos_c <chevisc@gmail.com>
2013-07-11 23:22:08 -0400

    web.js edit

--- web.js
+++ web.js
@@ -1,12 +1,5 @@
 var express = require('express');
-var fs - require('fs');
 var app = express.createServer(express.logger());
-
-var index = fs.readFileSync('index.html', function(err,data){
-	if(err) throw err;
-	console.log(data);
-});
-
 
 
 app.get('/', function(request, response) {
