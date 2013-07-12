4cf3756d2b92901a3a28c43ac9f65020ab16a21a
cardoos_c <chevisc@gmail.com>
2013-07-11 23:29:47 -0400

    web.js edit

--- web.js
+++ web.js
@@ -6,20 +6,23 @@ app.get('/', function(request, response) {
   response.send('hello world from html');
 });
 */
-var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('./index.html', function (err, html) {
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    index;
+ 
+fs.readFile('./index.html', function (err, data) {
     if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+        throw err;
+    }
+    index = data;
 });
+ 
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.close();
+}).listen(8000);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
